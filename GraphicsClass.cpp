2a4fba64fda3a078e4d35783a47b213bdc6dedc2
jph <fordivil@knou.ac.kr>
2020-03-17 07:36:23 +0900

    end

---
+++ GraphicsClass.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "graphicsclass.h"
+
+
+GraphicsClass::GraphicsClass()
+{
+}
+
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	return true;
+}
+
+
+void GraphicsClass::Shutdown()
+{
+}
+
+
+bool GraphicsClass::Frame()
+{
+	return true;
+}
+
+
+bool GraphicsClass::Render()
+{
+	return true;
+}
---
+++ GraphicsClass.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = true;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+};
